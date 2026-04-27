9c106e54a55e878049311c483de739f994c1c784
Nurudeen <nurdul4002@gmail.com>
2026-04-27 08:32:21 +0100

    remove files

--- test_client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/upload"
-
-files = {"image": open("C:\\Users\\user\\OneDrive\\Desktop\\30 for 30\\chatbot\\images\\car.jpg", "rb")}
-data = {"speed": "72"}
-
-res = requests.post(url, files=files, data=data)
-print(res.text)
